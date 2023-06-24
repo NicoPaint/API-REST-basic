1ce95d713f63553013d6b5018939af1c7bbca55a
NicoPaint <felipepinve@gmail.com>
2023-06-24 13:45:25 -0500

    Se agregó la funcionalidad al botón de nuevo gato

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ console.log('Hello world');
 
 const URL = 'https://api.thecatapi.com/v1/images/search';
 
+const catImg = document.querySelector('img');
+const catButton = document.querySelector('button');
+catButton.addEventListener('click', newCat);
+
+async function newCat(){
+    const res = await fetch(URL);
+    const data = await res.json();
+
+    catImg.src = data[0].url;
+}
+
+//con esta parte se muestra una imagen cuando se carga la página
 fetch(URL)
     .then(res => res.json())
     .then(data => {
