5634aba86f165347e8691e0cdf9f1b1ccab04530
NicoPaint <felipepinve@gmail.com>
2023-06-29 19:57:38 -0500

    Se agregó la API KEY y se modificó el HTML, CSS y JS para el proyecto con varias imagenes

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-console.log('Hello world');
+const URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_UhyG4bxBLmACFEMMq1LJPUbiuA2ZnMRTpEdKfLq9frYrWuomfpp6nl8AVBaJ0G35';
 
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const catImg1 = document.querySelector('.img1');
+const catImg2 = document.querySelector('.img2');
+const catImg3 = document.querySelector('.img3');
+const newCatButton = document.querySelector('.newCatButton');
+newCatButton.addEventListener('click', newCats);
 
-const catImg = document.querySelector('img');
-const catButton = document.querySelector('button');
-catButton.addEventListener('click', newCat);
-
-async function newCat(){
+async function newCats(){
     const res = await fetch(URL);
     const data = await res.json();
 
-    catImg.src = data[0].url;
+    console.log(data);
+    catImg1.src = data[0].url;
+    catImg2.src = data[1].url;
+    catImg3.src = data[2].url;
 }
 
 //con esta parte se muestra una imagen cuando se carga la página
-newCat();
+newCats();
