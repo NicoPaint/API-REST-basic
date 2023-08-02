3f6fc0886e416913501916a22ef275ca218c4a88
NicoPaint <felipepinve@gmail.com>
2023-08-02 18:23:11 -0500

    Se agrego la funcionalidad de actualizar la seccion de favoritos cada vez que se guarda un gato nuevo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_UhyG4bxBLmACFEMMq1LJPUbiuA2ZnMRTpEdKfLq9frYrWuomfpp6nl8AVBaJ0G35';
-const API_URL_FAVORITE = "https://api.thecatapi.com/v1/favourites?api_key=live_UhyG4bxBLmACFEMMq1LJPUbiuA2ZnMRTpEdKfLq9frYrWuomfpp6nl8AVBaJ0G35"
+const API_URL_FAVORITE = "https://api.thecatapi.com/v1/favourites?api_key=live_UhyG4bxBLmACFEMMq1LJPUbiuA2ZnMRTpEdKfLq9frYrWuomfpp6nl8AVBaJ0G35";
+const API_URL_FAVORITE_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/:${id}?api_key=live_UhyG4bxBLmACFEMMq1LJPUbiuA2ZnMRTpEdKfLq9frYrWuomfpp6nl8AVBaJ0G35`;
 const API_KEY = 'live_UhyG4bxBLmACFEMMq1LJPUbiuA2ZnMRTpEdKfLq9frYrWuomfpp6nl8AVBaJ0G35';
 
 const spanError = document.querySelector('#error');
@@ -44,37 +45,36 @@ async function loadFavoritesCats(){
     console.log('Favorites');
     console.log(data);
 
-    console.log(res.status);
-
     if(res.status !== 200){
         spanError.innerHTML = `Hubo un error ${res.status}`;
         console.log(res.status);
-        return;
     }
+    else{
+        favoriteSection.innerHTML = "";
 
-    data.forEach(favoriteCat => {
-        const img = document.createElement('img');
-        img.alt = 'Foto de Gatito Favorito';
-        img.src = favoriteCat.image.url;
-
-        const btnText = document.createTextNode('Eliminar de favoritos');
-        const btn = document.createElement('button');
-        btn.type = 'button';
-        btn.classList.add('buttonGreen')
-
-        const div = document.createElement('div');
-        div.classList.add('img-container');
-
-        const article = document.createElement('article');
-        article.classList.add('favoriteCat-card');
-
-        div.appendChild(img);
-        btn.appendChild(btnText);
-        article.appendChild(div);
-        article.appendChild(btn);
-        favoriteSection.appendChild(article);
-
-    })
+        data.forEach(favoriteCat => {
+            const img = document.createElement('img');
+            img.alt = 'Foto de Gatito Favorito';
+            img.src = favoriteCat.image.url;
+    
+            const btnText = document.createTextNode('Eliminar de favoritos');
+            const btn = document.createElement('button');
+            btn.type = 'button';
+            btn.classList.add('buttonGreen')
+    
+            const div = document.createElement('div');
+            div.classList.add('img-container');
+    
+            const article = document.createElement('article');
+            article.classList.add('favoriteCat-card');
+    
+            div.appendChild(img);
+            btn.appendChild(btnText);
+            article.appendChild(div);
+            article.appendChild(btn);
+            favoriteSection.appendChild(article);
+        })
+    }
 }
 
 async function saveFavoriteCats(id){
@@ -91,15 +91,36 @@ async function saveFavoriteCats(id){
     console.log('Save');
     console.log(res);
     
+    const data = await res.json();
+    console.log('sava data');
+    console.log(data);
+
     if(res.status !== 200){
         spanError.innerHTML = `Hubo un error ${res.status}`;
         console.log(res.status);
     }
+    else{
+        console.log('Gato favorito guardado exitosamente!');
+        loadFavoritesCats();
+    }
     
-    const data = await res.json();
-    console.log('sava data');
-    console.log(data);
 }
+
+/* async function deleteFavoriteCats(id){
+    const res = await fetch(API_URL_FAVORITE_DELETE(id),
+    {
+        method: 'DELETE',
+    })
+    const data = await res.json();
+
+    if(res.status !== 200){
+        spanError.innerHTML = `Hubo un error ${res.status}`;
+        console.log(res.status);
+    }
+    else{
+        console.log('Gato eliminado exitosamente!');
+    }
+} */
 
 //con esta parte se muestra una imagen cuando se carga la página
 newCats();
